b5e1b08e4992b411f8cf85942e7c197218589d5f
siddharthchugh <siddharthandroiddev@gmail.com>
2017-09-23 16:29:15 +0530

    Add files via upload

---
+++ DynamicRAWjsonActivity.java
@@ -0,0 +1,26 @@
+package tecnest.manage.mytaskproject.AllActivities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import tecnest.manage.mytaskproject.R;
+
+public class DynamicRAWjsonActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dynamic_rawjson);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+}
